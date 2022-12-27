2a433618ce7fc00589506d24264b901964d99397
paliszoman <kamilkrolik@hotmail.com>
2022-12-28 00:02:28 +0100

    new

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
